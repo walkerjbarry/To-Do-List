391242b4746a50e316d7ecf41a4f496ab6831652
barry_walker <barry.walker@esc13.txed.net>
2022-10-11 15:04:44 -0500

    Updated script - cleanup.

--- index.js
+++ index.js
@@ -77,19 +77,18 @@ function loadLocalToDo() {  /*This function will obtain the todos from local sto
         });
         const toDoDiv = document.getElementById("list");
         const newToDo = document.createElement("li");
-        toDoDiv.appendChild(newToDo);  /*here we append the element to whichever element we choose.  In this case, we are choosing to append the newly created list item to our newly created list element*/
+        toDoDiv.appendChild(newToDo);
 
         newToDo.innerText = task.name;
-        /*This is how we will display the text that the user has input for their tasks.  The.name is where we have selected the specific key ('name') of the object ('task')that we would like to display (within our 'newToDo'variable).  We previously were using a similar method, using .value.  The .value inputs the specific text (the "values" as it were) that the user types.*/
-        newToDo.classList.add('todo-item');  /**Here we are adding a class to the newly created html element.  This of course, will allow us to style our element. */
+        newToDo.classList.add('todo-item');  
 
         const removeBtn = document.createElement('button');
-        removeBtn.innerHTML = '<i class="fas fa-trash"></i>';/*here we use .innerHTML to add our button element as an icon, using the <i></i> element, and we use the class from the font - awesome cdn which we linked in the head of our html file.  Adding the button as an icon is obviously not critical, just a fun step for some styling.*/
+        removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
         removeBtn.classList.add('trash-btn');
         removeBtn.addEventListener("click", function () {
             removeBtn.remove(); newToDo.remove();
             removeLocalToDo(task)
-        });/*Onclick.our button will remove the newToDo fromthe GUI, and from localStorage.*/
+        });
         toDoDiv.appendChild(removeBtn);
     })
 };
