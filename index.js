de679d7cc7650bffce9d07f5e68755e1fdc49f67
barry_walker <barry.walker@esc13.txed.net>
2022-10-11 11:26:49 -0500

    Updated html, css, and script to debug preventDefault.

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-<<<<<<< HEAD
+
+
 
 function addToDo() {
-=======
->>>>>>> 7618aafe273424b85e82532c94a4aca4ed147e4d
 
-function addToDo(){ 
-  
 
-<<<<<<< HEAD
+
     const task = {
         name: document.getElementById("task").value,
         id: Date.now(), /*Here we have chosen to use the Date.now()method to generate a unique id for our tasks.*/
     };
     const toDo = document.getElementById("toDo");
 
-    event.preventDefault();
+    toDo.addEventListener("submit", function (event) {
+        event.preventDefault;
+    });
     //document.addEventListener("DOMToDosLoaded", addLocalToDo(task['id']));
     const toDoDiv = document.getElementById("list");
     const newToDo = document.createElement("li");
@@ -36,34 +35,7 @@ function addToDo(){
     toDoDiv.appendChild(removeBtn);
     task.value = ""; /*this will clear the input field for the next task input*/
     addLocalToDo(task);/*add todo to local storage*/
-=======
-  const task = {
-    name:document.getElementById("task").value,
-    id:Date.now(), /*Here we have chosen to use the Date.now()method to generate a unique id for our tasks.*/
-  };
-  const toDo = document.getElementById("toDo");
-  
-  event.preventDefault();
-  //document.addEventListener("DOMToDosLoaded", addLocalToDo(task['id']));
-  const toDoDiv = document.getElementById("list");
-  const newToDo = document.createElement("li");
-  toDoDiv.appendChild(newToDo);  /*here we append the element to whichever element we choose.  In this case, we are choosing to append the newly created list item to our newly created list element*/
- 
-  newToDo.innerText = task.name;
-  
-/*This is how we will display the text that the user has input for their tasks.  The.name is where we have selected the specific key ('name') of the object ('task')that we would like to display (within our 'newToDo'variable).  We previously were using a similar method, using .value.  The .value inputs the specific text (the "values" as it were) that the user types.*/
-  
-  newToDo.classList.add('todo-item');  /**Here we are adding a class to the newly created html element.  This of course, will allow us to style our element. */
-  
-  const removeBtn = document.createElement('button');
-  removeBtn.innerHTML='<i class="fas fa-trash"></i>';/*here we use .innerHTML to add our button element as an icon, using the <i></i> element, and we use the class from the font - awesome cdn which we linked in the head of our html file.  Adding the button as an icon is obviously not critical, just a fun step for some styling.*/
-  removeBtn.classList.add('trash-btn');
-  removeBtn.addEventListener("click", function() { removeBtn.remove(); newToDo.remove();     
-   removeLocalToDo(task)});/*Onclick.our button will remove the newToDo fromthe GUI, and from localStorage.*/
-  toDoDiv.appendChild(removeBtn);
-  task.value = ""; /*this will clear the input field for the next task input*/
-  addLocalToDo(task);/*add todo to local storage*/
->>>>>>> 7618aafe273424b85e82532c94a4aca4ed147e4d
+
 };
 
 function addLocalToDo(task){
@@ -79,7 +51,7 @@ function addLocalToDo(task){
 /*and we will also push other new todos to local storage*/
 };
 
-<<<<<<< HEAD
+
 function removeLocalToDo(task) {
     //const toDoDiv = document.getElementById("list");
     let toDoText;
@@ -109,7 +81,9 @@ function loadLocalToDo() {  /*This function will obtain the todos from local sto
         id:Date.now(), /*Here we have chosen to use the Date.now()method to generate a unique id for our tasks.
       };*/
         const toDo = document.getElementById("toDo");
-
+        toDo.addEventListener("load", function (event) {
+            event.preventDefault;
+        });
         event.preventDefault();
         const toDoDiv = document.getElementById("list");
         const newToDo = document.createElement("li");
@@ -131,7 +105,7 @@ function loadLocalToDo() {  /*This function will obtain the todos from local sto
         toDoDiv.appendChild(removeBtn);
     })
 };
-=======
+
 function removeLocalToDo(task){
   //const toDoDiv = document.getElementById("list");
   let toDoText;
@@ -148,7 +122,7 @@ if(localStorage.getItem('toDoText') !== null){
   localStorage.setItem('toDoText', JSON.stringify(toDoText));//Here we reset the updated array in localStorage
 };
 function loadLocalToDo(){  /*This function will obtain the todos from local storage, and use them to poupulate the UI, and populte the local storage onload.  Code and notes notes are pasted from above code.*/
->>>>>>> 7618aafe273424b85e82532c94a4aca4ed147e4d
+
 
     let toDoText;  
   if(localStorage.getItem('toDoText') === null){
@@ -183,4 +157,3 @@ function loadLocalToDo(){  /*This function will obtain the todos from local stor
   })
 };
 
-  
