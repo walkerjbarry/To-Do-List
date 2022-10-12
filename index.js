5d0bec82b5f6ddb47d371b7499ea16faa3f6a26d
barry_walker <barry.walker@esc13.txed.net>
2022-10-12 11:17:53 -0500

    Code cleanup, implementing 'DRY' principle.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function addToDo() {
+function addToDo(event) {
+
     const task = {
         name: document.getElementById("task").value,
         id: Date.now(), /*Here we have chosen to use the Date.now()method to generate a unique id for our tasks.*/
@@ -6,40 +7,39 @@ function addToDo() {
     addLocalToDo(task);//add todo to local storage
     addToDoToUI(task);//populate the UI with todos from localStorage
 };
+//EVENT LISTENERS
 
-function addToDoToUI(task) {
-    const toDo = document.getElementById("toDo");
-    toDo.addEventListener("submit", function (event) {
+removeBtn.addEventListener("click", function () {
+    removeBtn.remove(); newToDo.remove();
+    removeLocalToDo(task)
+    });/*Onclick.our button will remove the newToDo fromthe GUI, and from localStorage.*/
+
+toDo.addEventListener("submit", function (event) {
         event.preventDefault;
     });
+
+function addToDoToUI(task) {
+
+    const toDo = document.getElementById("toDo");
     const toDoDiv = document.getElementById("list");
     const newToDo = document.createElement("li");
     toDoDiv.appendChild(newToDo);  /*here we append the element to whichever element we choose.  In this case, we are choosing to append the newly created list item to our newly created list element*/
     newToDo.innerText = task.name;
     /*This is how we will display the text that the user has input for their tasks.  The.name is where we have selected the specific key ('name') of the object ('task')that we would like to display (within our 'newToDo'variable).  We previously were using a similar method, using .value.  The .value inputs the specific text (the "values" as it were) that the user types.*/
     newToDo.classList.add('todo-item');  /**Here we are adding a class to the newly created html element.  This of course, will allow us to style our element. */
-
     const removeBtn = document.createElement('button');
     removeBtn.innerHTML = '<i class="fas fa-trash"></i>';/*here we use .innerHTML to add our button element as an icon, using the <i></i> element, and we use the class from the font - awesome cdn which we linked in the head of our html file.  Adding the button as an icon is obviously not critical, just a fun step for some styling.*/
-    removeBtn.classList.add('trash-btn');
-    removeBtn.addEventListener("click", function () {
-        removeBtn.remove(); newToDo.remove();
-        removeLocalToDo(task)
-    });/*Onclick.our button will remove the newToDo fromthe GUI, and from localStorage.*/
+    removeBtn.classList.add('trash-btn'); 
     toDoDiv.appendChild(removeBtn);
-    task.value = ""; /*this will clear the input field for the next task input*/
 };
 
-function addLocalToDo(task){
-  let toDoText;  
-  if(localStorage.getItem('toDoText') === null){
-    toDoText = [];/*here we check if we already have a todo array, and if not, we will create an empty array*/
-  }else{
-    toDoText = JSON.parse(localStorage.getItem('toDoText'));
-  }/*and if we DO have a todo array, we will return it as a parsed array*/
-  toDoText.push(task); 
-  
-  localStorage.setItem('toDoText',JSON.stringify(toDoText));
+task.value = "";/*this will clear the input field for the next task input*/
+
+function addLocalToDo(task) {
+
+  getFromLocalStorage();
+  toDoText.push(task);  
+  setLocalStorage();
 /*and we will also push other new todos to local storage*/
 };
 
@@ -50,45 +50,34 @@ function removeLocalToDo(task) {
         toDoText = JSON.parse(localStorage.getItem('toDoText'));
     }
     const toDoIndex = task.id;/*Creating this variable to be equal to the value we're looking to find, is how we can find the index position and remove it (via the splice() method in the next step)*/
-    for (let i = 0; i < toDoText.length; i++) {  /*This foor loop and the logic below is how we will iterate through our array to find our value (to be removed in this case)*/
-            if (toDoIndex === toDoText[i].id) {
-            toDoText.splice(i, 1);
-        }
+    for (let i = 0; i < toDoText.length; i++) {  /*This for loop and the logic below is how we will iterate through our array to find our value (to be removed in this case)*/
+        if (toDoIndex === toDoText[i].id) {
+            toDoText.splice(i, 1)
+        };/*Here we declare what element(s) we want to splice (i), and how many elements (1)*/
     };
-    /*Here we declare what element(s) we want to splice (i), and how many elements (1)*/
-    localStorage.setItem('toDoText', JSON.stringify(toDoText));//Here we reset the updated array in localStorage
+    setLocalStorage();
 };
 
-function loadLocalToDo() {  /*This function will obtain the todos from local storage, and use them to poupulate the UI, and populte the local storage onload.  Code and notes notes are pasted from above code.*/
+function getFromLocalStorage(toDoText) {
 
     let toDoText;
     if (localStorage.getItem('toDoText') === null) {
         toDoText = [];/*here we check if we already have a todo array, and if not, we will create an empty array*/
     } else {
-        toDoText = JSON.parse(localStorage.getItem('toDoText'));
+        toDoText = JSON.parse(localStorage.getItem('toDoText'));/*and if we DO have a todo array, we will return it as a parsed array*/
     }
-    toDoText.forEach(function (task) {
+}
 
-        const toDo = document.getElementById("toDo");
-        toDo.addEventListener("load", function (event) {
-            event.preventDefault;
-        });
-        const toDoDiv = document.getElementById("list");
-        const newToDo = document.createElement("li");
-        toDoDiv.appendChild(newToDo);
+function setLocalStorage() {
 
-        newToDo.innerText = task.name;
-        newToDo.classList.add('todo-item');
-
-        const removeBtn = document.createElement('button');
-        removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
-        removeBtn.classList.add('trash-btn');
-        removeBtn.addEventListener("click", function () {
-            removeBtn.remove(); newToDo.remove();
-            removeLocalToDo(task)
-        });
-        toDoDiv.appendChild(removeBtn);
-    });
+    localStorage.setItem('toDoText', JSON.stringify(toDoText)); //Here we reset the updated array in localStorage
 };
+
+function loadLocalToDo() {  /*This function will obtain the todos from local storage, and use them to poupulate the UI, and populte the local storage onload.  Code and notes notes are pasted from above code.*/
+
+    getFromLocalStorage();
+    toDoText.forEach(function (task) {
+        addToDoToUI();
+    })};
 
 
