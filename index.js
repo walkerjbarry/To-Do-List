4adde5f9d35c0fe9c4f1429ed44319cff367b53d
barry_walker <barry.walker@esc13.txed.net>
2022-10-20 14:27:32 -0500

    Code cleanup

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-/*A user must be able to add and delete a todo item, from the UI and from your storage.
--Save each todo item as an object, with a key for the todo text and an "id" key that represents a random number you can use to search it by. So, you should have an array of objects that you're saving in localStorage.
--Hint: use JavaScript localStorage to store the todos.
--Hint: To convert an array into the string representation, you'll need JSON stringify. To convert it back to an array, you'll need JSON parse.
--Send me the project for review and get started on the next project.
-You can store your Todos in an array, but keep in mind that localStorage can only store strings, so that means you'll have to convert your array of todos into a string before you can store it. And when you retrieve it, you have to convert it from a string back to an array. Those are all things you can do with JavaScript, so make sure to look those up.
-Also, you can use JavaScript to add an "onclick" attribute to elements you're using JavaScript to create. This may be of note when thinking of how to delete the todo element.
------------------------------------------
-COMPONENTS NEEDED:
-items saved as objects,
-    with a key (toDo text)
-    and ID key
-        which is a random number*/
+/*Add a new function to your ToDos app that initially loads ToDos data from an API.
+
+
+-Create a new Git BRANCH, and make the updates for Project 9 in this branch. Keep Project 8 in the initial branch (main/master).
+-Update the ToDos app from the previous project to initially load the localStorage of ToDos by fetching the ToDos resources in the JSONPlaceholder API using Axios, NOT the "fetch" example that's in the JSONPlaceholder API documentation.
+-Only store 5 todos from the API instead of the whole thing, just to pre-populate it with a few items.
+-Use the Axios API. On the page load, if the Todos array is empty, load it with the Todos from this API. Otherwise, if it's not empty, it should load what's currently stored.*/
+ 
 
 function addToDo(event) {
 
